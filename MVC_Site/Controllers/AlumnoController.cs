e03d649d159c4eec7666ceef8aae73dd980c23b5
= <lagarcia2015url@gmail.com>
2019-01-25 22:16:04 -0600

    Primer commit

---
+++ MVC_Site/Controllers/AlumnoController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Site.Models;
+
+namespace MVC_Site.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        // GET: Alumno
+        public ActionResult Index()
+        {
+            List<Alumno> listaalumnos = new List<Alumno>();
+
+            Alumno A = new Alumno();
+            A.nombreCompleto = "Lester Andrés García Aquino";
+            A.carne = 1003115;
+            A.fechaNacimiento = Convert.ToDateTime("1997-02-13");
+
+            Alumno B = new Alumno();
+            B.nombreCompleto = "Lester Andrés García Aquino";
+            B.carne = 1003116;
+            B.fechaNacimiento = Convert.ToDateTime("1997-02-14");
+
+            listaalumnos.Add(A);
+            listaalumnos.Add(B);
+
+            return View(listaalumnos);
+        }
+
+        // GET: Alumno/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Alumno/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Alumno/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Alumno/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Alumno/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Alumno/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Alumno/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVC_Site/Models/Alumno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Site.Models
+{
+    public class Alumno
+    {
+        public string nombreCompleto { get; set; }
+        public int carne { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+    }
+}
